b791e6bb0765ee144cc2bbaff1ef68190a2b3dfb
Steph-duryumsu <yetbudam@gmail.com>
2025-05-22 12:53:59 +0100

    nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import Navbar from './components/Navbar';
 
-// src/App.jsx
-export default function App() {
+
+
+function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600 text-white">
-      <h1 className="text-4xl font-bold">Welcome to DevConnect 👋</h1>
+    <div>
+      <Navbar/>
+      
     </div>
   );
 }
 
+export default App;
